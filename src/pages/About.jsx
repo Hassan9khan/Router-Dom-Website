fc3eeab638bdeb480b3932d1c3f3fe1c886bbc46
Hassan Ahmed <hassanshakeel381@gmail.com>
2024-09-12 23:10:15 +0500

    updated

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,7 @@ import { Link , Outlet } from 'react-router-dom'
 const About = () => {
   return (
     <>
-    <div>About</div>
+    <h1>About</h1>
     <Link to="">Person 1</Link>
     <Link to="person2">Person 2</Link>
     <Link to="person3">Person 3</Link>
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <div><h1>Services</h1></div>
   )
 }
 

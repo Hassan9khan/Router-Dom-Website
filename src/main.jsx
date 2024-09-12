435ee24ab8ff57a07791382ce1a99cea510e8127
Hassan Ahmed <hassanshakeel381@gmail.com>
2024-09-12 23:05:33 +0500

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import Layout from "./Layout.jsx";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 import Services from "./pages/Services.jsx";
-import Contact from "./pages/Contact.jsx";
+// import Contact from "./pages/Contact.jsx";
 
 const router = createBrowserRouter([
   {
@@ -22,35 +22,34 @@ const router = createBrowserRouter([
         children: [
           {
             path: "",
-            element: <h1>Person 1</h1>
+            element: <h1>Person 1</h1>,
           },
           {
             path: "person2",
-            element: <h1>Person 2</h1>
+            element: <h1>Person 2</h1>,
           },
           {
             path: "person3",
-            element: <h1>Person 3</h1>
+            element: <h1>Person 3</h1>,
           },
-        ]
+        ],
       },
-      {
-        path: "contact",
-        element: <Contact />,
-      },
+      // {
+      //   path: "contact",
+      //   element: <Contact />,
+      // },
       {
         path: "services",
         element: <Services />,
       },
       {
         path: "*",
-        element: <h1>Not Found</h1>
-      }
+        element: <h1>Not Found</h1>,
+      },
     ],
   },
 ]);
 
 createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router}>
-  </RouterProvider>
-);  
+  <RouterProvider router={router}></RouterProvider>
+);
